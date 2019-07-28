cb64a8badcf5e29052a0515d9c125fb87aecc8be
Sergi Navarro <46789043+senavarro@users.noreply.github.com>
2019-07-28 11:46:35 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import FetchExample from "./sections/fetch-example"
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <FetchExample />
+      </div>
+    );
+  }
+}
+
+
+export default App;
